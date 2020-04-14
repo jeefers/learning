d3e247595b84fda4ada47347e8741b8572962f1b
Jeff <jeefers00@gmail.com>
2020-04-14 01:29:17 -0400

    hello world

---
+++ hello.py
@@ -0,0 +1,3 @@
+# This is some files that im using to learn different tech.
+# I currently am not using git but want to keep my skills sharp with it thus why i created a repot for it.
+print ('hello world')
